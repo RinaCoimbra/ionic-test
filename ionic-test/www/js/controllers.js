fa3b3705249dead79912a6452e924f5d692c709f
rinacoimbra <rina.coimbra@gmail.com>
2016-01-25 09:01:40 +0000

    ionic imported from ionic creator

---
+++ ionic-test/www/js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+  
+.controller('pageCtrl', function($scope) {
+    $scope.bleh = "bleeeeeeh";
+})
+   
+.controller('cameraTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('cartTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('cloudTabDefaultPageCtrl', function($scope) {
+
+})
+    

44fd20eec7f438ca31d937af0890ef71733ae5c9
rinacoimbra <rina.coimbra@gmail.com>
2016-01-26 12:52:14 +0000

    Review Angular

--- ionic-test/www/js/controllers.js
+++ ionic-test/www/js/controllers.js
@@ -1,11 +1,36 @@
 angular.module('app.controllers', [])
   
-.controller('pageCtrl', function($scope) {
-
+.controller('pageCtrl', function($scope, $http) {
+    $scope.fantasma = [
+        {"nome":"Gengar", "tipo":"Assustador", "podeservisto": true},
+        {"nome": "Gasparzinho", "tipo":"Camarada", "podeservisto": true},
+        {"nome": "Poltergeist", "tipo":"Fenomeno", "podeservisto": false},
+        {"nome": "Homem de Marshmallow", "tipo":"Gigante", "podeservisto":true}
+        ];
+        
+    $scope.buscaAPI = function(busca2){
+        // Simple GET request example:
+        var apikey = "7c20378c-001e-4639-ab96-669be9f17f7f";
+        var url = "https://na.api.pvp.net/api/lol/br/v1.4/summoner/by-name/"
+        var nome = busca2;
+        console.log(nome);
+        var resto_da_url = "?api_key=";
+        var url_get = url + nome + resto_da_url + apikey;
+        $http.get(url_get).then(function successCallback(response) {
+            console.log(response);
+          }, function errorCallback(response) {
+            alert(JSON.stringify(response));
+          });
+    };
 })
    
 .controller('cameraTabDefaultPageCtrl', function($scope) {
-    $scope.bleh = "hey";
+   // $scope.bleh = "bleeeeeeeeeeeeh";
+    $scope.bleh = {"blih": "blaaaaaaaah"};
+    $scope.animaisMarinhos = ["Baleia", "Golfinho", "Tubarão"];
+    $scope.alertbleh = function(){
+        alert("bleeeeeeh");
+    }
 })
    
 .controller('cartTabDefaultPageCtrl', function($scope) {
